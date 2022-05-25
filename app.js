6706589c81383dd967eb708379a3405c4c7b9779
ughurba <ughurba@edu.code.az>
2022-05-25 16:37:20 +0400

    refactoring code

--- app.js
+++ app.js
@@ -7,31 +7,34 @@ let marginTopBottom = 10
 
 document.addEventListener('keydown',function (e){
 
-    if(e.keyCode === 39){
+const keyCodeHandlers = {
+
+    39:()=>{
         margin +=5
         block.style.marginLeft = `${margin}px`
-
-
-    }
-    if(e.keyCode === 37){
-
-
+    },
+    37:()=>{
         margin -=5
         block.style.marginLeft = `${margin}px`
-    }
-
-    if(e.keyCode === 38){
-
+    },
+    38:()=>{
         marginTopBottom -=5
         block.style.marginTop = `${marginTopBottom}px`
-    }
-
-    if(e.keyCode === 40){
-
-
+    },
+    40:()=> {
         marginTopBottom +=5
         block.style.marginTop = `${marginTopBottom}px`
+    },
 
-    }
+
+}
+
+keyCodeHandlers[e.keyCode]();
+
+
+
+
+
+
 
 })
