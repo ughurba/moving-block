2b57f1c2eedc1773961b646c4da582b8505ffdf0
ughurba <ughurba@edu.code.az>
2022-05-25 15:18:46 +0400

    refactoring code

--- app.js
+++ app.js
@@ -1,73 +1,37 @@
 const block = document.querySelector('.block')
 // 39-right 37-left 38-top 40-bottom
-let margin = '10px'
-let marginTopBottom = '10px'
+let margin = 10
+let marginTopBottom = 10
 
 
 
 document.addEventListener('keydown',function (e){
 
     if(e.keyCode === 39){
-        let number =  margin.slice(0,margin.length-2)
-        number++;
-        number++;
-        margin = number
-        margin ='px'
-        number += margin
-        console.log(number)
-        margin = number
-
-        block.style.marginLeft = margin
-
-
+        margin +=5
+        block.style.marginLeft = `${margin}px`
 
 
     }
     if(e.keyCode === 37){
 
-        number =  margin.slice(0,margin.length-2)
-        number--;
-        number--;
-        margin = number
-        margin ='px'
-        number += margin
-        console.log(number)
-        margin = number
 
-       // block.style.position = 'relative'
-        block.style.marginLeft =margin
+        margin -=5
+        block.style.marginLeft = `${margin}px`
     }
 
     if(e.keyCode === 38){
 
-
-        number =  marginTopBottom.slice(0,marginTopBottom.length-2)
-        number--;
-        number--;
-        marginTopBottom = number
-        marginTopBottom ='px'
-        number += marginTopBottom
-        console.log(number)
-        marginTopBottom = number
-
-        // block.style.position = 'relative'
-        block.style.marginTop = marginTopBottom
+        marginTopBottom -=5
+        block.style.marginTop = `${marginTopBottom}px`
     }
 
     if(e.keyCode === 40){
 
 
-        number =  marginTopBottom.slice(0,marginTopBottom.length-2)
-        number++;
-        number++;
-        marginTopBottom = number
-        marginTopBottom ='px'
-        number += marginTopBottom
-        console.log(number)
-        marginTopBottom = number
+        marginTopBottom +=5
+        block.style.marginTop = `${marginTopBottom}px`
 
-        // block.style.position = 'relative'
-        block.style.marginTop = marginTopBottom
     }
 
 })
